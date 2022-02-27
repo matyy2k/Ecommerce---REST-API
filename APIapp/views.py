80552c1e8095882859e4d49b4dfbb218d71be0b1
matyy2k <49438936+matyy2k@users.noreply.github.com>
2022-02-27 22:40:32 +0100

    test new branch

--- APIapp/views.py
+++ APIapp/views.py
@@ -6,6 +6,8 @@ from .serializers import BookSerializers, CategorySerializers, ProductSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework import viewsets
 
+# test
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
