1088948502972766ecfdeaac59861cd975118344
matyy2k <matim20075@gmail.com>
2022-03-03 23:30:04 +0100

    fix env file

--- EcommerceAPI/settings.py
+++ EcommerceAPI/settings.py
@@ -21,11 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = config('DEBUG', default=False)
 
 ALLOWED_HOSTS = ['https://ecommerce-restapi-djangoapp.herokuapp.com',
                  'localhost:8000',
-                 'ecommerce-restapi-djangoapp.herokuapp.com',]
+                 'ecommerce-restapi-djangoapp.herokuapp.com',
+                 '127.0.0.1', ]
 
 
 # Application definition
