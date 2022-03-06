ca93b5e854fd884fe0d7b5e273b062e8641cda2d
matyy2k <matim20075@gmail.com>
2022-03-06 22:49:33 +0100

    fix slash

--- APIapp/urls.py
+++ APIapp/urls.py
@@ -13,13 +13,13 @@ urlpatterns = [
     path('', include(router.urls)),
     path('categories/', ListCategory.as_view(), name='categories'),
     path('categories/<int:pk>/', DetailCategory.as_view(), name='singlecategory'),
-    path('books', ListBook.as_view(), name='books'),
+    path('books/', ListBook.as_view(), name='books'),
     path('books/<int:pk>/', DetailBook.as_view(), name='singlebook'),
-    path('products', ListProduct.as_view(), name='products'),
+    path('products/', ListProduct.as_view(), name='products'),
     path('products/<int:pk>/', DetailProduct.as_view(), name='singleproduct'),
     path('auth/', obtain_auth_token),
     path('carts/', ListCart.as_view(), name='allcarts'),
-    path('carts/<int:pk>', DetailCart.as_view(), name='cartdetail'),
+    path('carts/<int:pk>/', DetailCart.as_view(), name='cartdetail'),
 
 ]
 
